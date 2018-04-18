94e790901db5907e424b5921e916ab0cc15f4809
Collin Timothy Moore <moorect@dhcp-137-112-104-243.rose-hulman.edu>
2018-04-17 21:13:15 -0400

    can obtain all the urls for the armor pieces

--- WebScrapper/scrapper.py
+++ WebScrapper/scrapper.py
@@ -1,11 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import numpy as np
+import re
 
-url = 'http://kiranico.com/index.php/en/mh3u/weapon/greatsword'
-r = requests.get(url)
+def get_armor_links(url):
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, 'lxml')
+    a_tags = soup.find_all(href=lambda x : x and re.compile(url).search(x))
+    links = list(map(lambda x : x['href'], a_tags))
+    return links
 
-data = r.text
+def get_all_armor_links():
+    # Armor list urls by body piece
+    heads = 'http://kiranico.com/en/mh4u/armor/head'
+    chest = 'http://kiranico.com/en/mh4u/armor/chest'
+    arms = 'http://kiranico.com/en/mh4u/armor/arms'
+    waist = 'http://kiranico.com/en/mh4u/armor/waist'
+    legs = 'http://kiranico.com/en/mh4u/armor/legs'
 
-soup = BeautifulSoup(data, 'lxml')
+    master_list = []
+    urls = [heads, chest, arms, waist, legs]
+    for u in urls:
+        master_list += get_armor_links(u)
+    return master_list
 
-print(soup.find_all('tr'))
+array = get_all_armor_links()
+for w in array:
+    print(w)
