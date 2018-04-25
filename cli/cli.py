ce94d97443445c8947b95dd5aa4827ac33a21c9c
Rahul Yarlagadda <knufire@gmail.com>
2018-04-25 02:16:53 -0400

    More work on redisDriver and cli

--- cli/cli.py
+++ cli/cli.py
@@ -1,7 +1,57 @@
 from click_shell import shell 
 import click as c
+from redis_ import redisDriver
+
+r = redisDriver.RedisDriver()
 
 @shell(prompt= 'BuildHunter> ', intro='Welcome to BuildHunter!')
 def cli():
-    pass
+    username = c.prompt('What is your username?')
+    if r.is_user(username):
+        r.active_user = username
+    else:
+        print(username + ' not found. Creating...')
+        r.add_user(username, True)
+
+@cli.command('username')
+def get_active_user():
+    print(r.active_user)
+
+@c.option('--name', '-n', prompt=True)
+@cli.command('build-add')
+def add_build(name):
+    r.add_build(name)
+
+@c.option('--name', '-n', prompt=True)
+@cli.command('build-delete')
+def delete_build(name):
+    r.delete_build(name)
+
+@cli.command('builds')
+def get_builds():
+    for build in r.get_all_builds():
+        print(build.decode('utf-8').partition(':')[2])
+
+@c.option('--name', '-n', prompt=True)
+@cli.command('build-set-active')
+def set_active_build(name):
+    r.active_build = name
+
+@cli.command('build-get-active')
+def get_active_build():
+    print(r.active_build)
+
+@c.option('--part', '-p', type=c.Choice(r.BUILD_PARTS), prompt=True)
+@c.option('--id', '-i', type=int, prompt=True)
+@cli.command('part-add')
+def add_part(part, id):
+    r.add_build_component(part, id)
+    
+@c.option('--part', '-p', type=c.Choice(r.BUILD_PARTS), prompt=True)
+@cli.command('part-delete')
+def delete_part(part):
+    r.remove_build_component(part)
+
+
+
 
--- redis_/redisDriver.py
+++ redis_/redisDriver.py
@@ -2,7 +2,8 @@ import redis
 
 class RedisDriver:
 
-    _r = redis.StrictRedis(host='433-05.csse.rose-hulman.edu', port=6379, db=0, password='huntallthemonsters247')
+    # _r = redis.StrictRedis(host='433-05.csse.rose-hulman.edu', port=6379, db=0, password='huntallthemonsters247')
+    _r = redis.StrictRedis()
 
     def __init__(self):
         self._active_user = None
@@ -12,6 +13,8 @@ class RedisDriver:
 
     @property
     def active_user(self):
+        if self._active_user is None:
+            raise ValueError('No active user set.')
         return self._active_user
 
     @active_user.setter
@@ -24,16 +27,19 @@ class RedisDriver:
     @active_user.deleter
     def active_user(self):
         self._active_user = None
+        self._active_build = None
 
     @property
     def active_build(self):
+        if self._active_build is None:
+            raise ValueError('No active build set.')
         return self._active_build
     
     @active_build.setter
     def active_build(self, build):
         temp = self._active_build
         self._active_build = build
-        if self._r.sismember(self.active_user + ":builds", self.get_build_id()):
+        if not self._r.sismember(self.active_user + ':builds', self.get_build_id()):
             self._active_build = temp
             raise ValueError('Build does not exist for the current active user')
             
@@ -42,11 +48,7 @@ class RedisDriver:
         self._active_build = None
     
     def get_build_id(self):
-        if self.active_user is None:
-            raise ValueError('No active user set.')
-        if self.active_build is None:
-            raise ValueError('No active build set.')
-        return self.active_user + ':build:' + self.active_build
+        return self.active_user + ':' + self.active_build
 
     ####----Users----####
     
@@ -60,19 +62,33 @@ class RedisDriver:
         #TODO
         pass
 
+    def is_user(self, user):
+        return self._r.sismember('users', user)
+
     ####----Builds----####
     
-    def add_build(self, build, setActive=False):
-        #TODO
-        pass
+    def add_build(self, build):
+        buildId = self.active_user + ':' + build
+
+        #Add build to user builds
+        self._r.sadd(self.active_user + ":builds", buildId)
     
     def delete_build(self, build):
-        #TODO
-        pass
+        buildId = self.active_user + ':' + build
+        if build == self.active_build:
+            del self.active_build
+        #Remove from user build list
+        self._r.srem(self.active_user + ":builds", buildId)
+
+        #Delete build hash
+        self._r.delete(buildId)
+
+        #Delete decoration sets
+        for part in self.BUILD_PARTS:
+            self._r.delete(buildId + ':' + part)
 
     def get_all_builds(self):
-        #TODO
-        pass
+        return self._r.smembers(self.active_user + ':builds')
     
     def get_build_details(self):
         #TODO
@@ -83,26 +99,22 @@ class RedisDriver:
     BUILD_PARTS = ['head', 'chest', 'arms', 'waist', 'legs', 'weapon']
 
     def add_build_component(self, part, itemId):
-        #TODO
-        pass
+        self._r.hset(self.get_build_id(), part, itemId)
 
     def remove_build_component(self, part):
-        #TODO
-        pass
+        self._r.hdel(self.get_build_id(), part)
+        self._r.delete(self.get_build_id() + ':' + part)
     
     ####----Decorations----####
 
     def add_decoration(self, part, itemId):
-        #TODO
-        pass
+        self._r.sadd(self.get_build_id + ':' + part, itemId)
     
     def remove_decoration(self, part, itemId):
-        #TODO
-        pass
+        self._r.srem(self.get_build_id + ':' + part, itemId)
 
     def remove_all_decorations(self, part):
-        #TODO
-        pass
+        self._r.delete(self.get_build_id + ':' + part)
 
     ####----Items----####
 
