a614f841842061c1a5ed5f0661e6de8f2ce171e2
cKeinsley <keinsleyChris@gmail.com>
2018-04-26 22:05:55 -0400

    skipping dummy objects

--- cassandra/cassandra_armor_insert.py
+++ cassandra/cassandra_armor_insert.py
@@ -22,6 +22,8 @@ def main():
     print("Load Complete")
     for armor in armor_item_list:
         print("Processing armor piece " + armor.get('Name'))
+        if 'dummy' in armor.get('Name')
+            pass
         main_armor = convertArmor(armor)
         skills = convertSkills(armor)
         crafting = convertCrafting(armor)
