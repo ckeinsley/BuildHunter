1c5db32df230b43c763d4c17b9fe6be055598a8e
Rahul Yarlagadda <knufire@gmail.com>
2018-05-03 20:01:27 -0400

    More features added to redisDriver and cli

--- redis_/redisAdmin.py
+++ redis_/redisAdmin.py
@@ -6,10 +6,13 @@ _r = redis.StrictRedis(host='433-05.csse.rose-hulman.edu', port=6379, db=0, pass
 def import_data(filename):
     file = open(filename, 'rb')
     data = pickle.load(file, encoding='unicode')
-    types = ['armor', 'weapon', 'skill', 'item']
+    #types = ['armor', 'weapon', 'skill', 'item']
+    types = ['item', 'decoration']
     for key, value in data.items():
         for type_ in types:
             if key.startswith(type_.upper()):
                 _r.hset(type_+'_ids', value, key.partition(':')[2])
                 _r.hset(type_+'_names', key.partition(':')[2], value)
 
+
+import_data('/home/yarlagrt/Documents/CSSE433/BuildHunter/WebScrapper/obj/name_id_map.p')
