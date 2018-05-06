7ba2075c870b0c1e3efff500f603326e5db94f12
cKeinsley <keinsleyChris@gmail.com>
2018-05-06 16:45:42 -0400

    tried switching to confluent kafka

--- kafka/consumer_test.py
+++ kafka/consumer_test.py
@@ -1,18 +1,34 @@
-from kafka import KafkaConsumer
+from confluent_kafka import Consumer, KafkaError
 import json
 
 topic = "armor-insert"
+settings = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'buildHunter',
+    'client.id': 'cassandra',
+    'enable.auto.commit': False,
+    'session.timeout.ms': 6000,
+    'default.topic.config': {'auto.offset.reset': 'smallest'}
+}
 
-def main():
-    consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-                            auto_offset_reset='earliest',
-                            consumer_timeout_ms=1000)
-    consumer.subscribe(['armor-insert'])
-    while(True):
-        msg = consumer.poll()
-        if not msg:
+c = Consumer(settings)
+c.subscribe([topic])
+
+try:
+    while True:
+        msg = c.poll(0.1)
+        if msg is None:
             continue
-        print(msg)
+        elif not msg.error():
+            print('Received message: {0}'.format(msg.value()))
+        elif msg.error().code() == KafkaError._PARTITION_EOF:
+            print('End of partition reached {0}/{1}'
+                  .format(msg.topic(), msg.partition()))
+        else:
+            print('Error occured: {0}'.format(msg.error().str()))
 
-if __name__ == "__main__":
-    main()
+except KeyboardInterrupt:
+    pass
+
+finally:
+    c.close()
