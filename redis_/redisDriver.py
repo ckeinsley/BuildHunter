cc11455938fedb5bdb132187e202bb6c066bc03e
Collin Timothy Moore <collin.moore96@gmail.com>
2018-05-06 23:13:54 -0400

    think I separated the state junk from the redis driver other than the redis instance. Need to test

--- redis_/redisDriver.py
+++ redis_/redisDriver.py
@@ -2,18 +2,10 @@ import redis
 
 class RedisDriver:
 
-    _r = None
-    #_r = redis.StrictRedis(host='433-05.csse.rose-hulman.edu', port=6379, db=0, password='huntallthemonsters247')
+    _r = redis.StrictRedis(host='433-05.csse.rose-hulman.edu', port=6379, db=0, password='huntallthemonsters247')
     #_r = redis.StrictRedis()
 
-    def __init__(self):
-        self._active_user = None
-        self._active_build = None
-
-    def connect(self):
-        self._r = redis.StrictRedis(host='433-05.csse.rose-hulman.edu', port=6379, db=0, password='huntallthemonsters247')
-
-    def check_alive(self):
+    def ping(self):
         try:
             response = self._r.ping()
             return response
@@ -23,102 +15,54 @@ class RedisDriver:
 
     ####----active_user and active_build properties----####
 
-    @property
-    def active_user(self):
-        if self._active_user is None:
-            raise ValueError('No active user set.')
-        return self._active_user
-
-    @active_user.setter
-    def active_user(self, user):
-        if self._r.sismember('users', user):
-            self._active_user = user
-        else:
-            raise ValueError(user + ' is not a registered user.')
-
-    @active_user.deleter
-    def active_user(self):
-        self._active_user = None
-        self._active_build = None
-
-    @property
-    def active_build(self):
-        if self._active_build is None:
-            raise ValueError('No active build set.')
-        return self._active_build
+    def is_user(self, user):
+        return self._r.sismember('users', user)
     
-    @active_build.setter
-    def active_build(self, build):
-        temp = self._active_build
-        self._active_build = build
-        if not self._r.sismember(self.active_user + ':builds', self.get_build_id()):
-            self._active_build = temp
-            raise ValueError('Build does not exist for the current active user')
-            
-    @active_build.deleter
-    def active_build(self):
-        self._active_build = None
-    
-    def get_build_id(self):
-        return self.active_user + ':' + self.active_build
+    def build_exists_for_user(self, user, build_id):
+        return self._r.sismember(user + ':builds', build_id)
 
     ####----Users----####
-    
-    def add_user(self, user, setActive=False):
-        if (self._r.sismember('users', user)):
-            raise ValueError('User ' + user +  ' already exists')
-        else:
-            self._r.sadd('users', user)
-            if setActive:
-                self.active_user = user
+
+    def add_user(self, user):
+        self._r.sadd('users', user)
 
     def delete_user(self, user):
-        if (user is self.active_user):
-            del self.active_user
         self._r.delete(*self._r.keys(user + ':*'))
         self._r.srem('users', user)
 
-    def is_user(self, user):
-        return self._r.sismember('users', user)
-
     ####----Builds----####
     
-    def add_build(self, build):
-        buildId = self.active_user + ':' + build
-
+    def add_build(self, user, buildId):
         #Add build to user builds
-        self._r.sadd(self.active_user + ":builds", buildId)
+        self._r.sadd(user + ":builds", buildId)
     
-    def delete_build(self, build):
-        buildId = self.active_user + ':' + build
-        if build == self.active_build:
-            del self.active_build
+    def delete_build(self, user, buildId, build_parts):
         #Remove from user build list
-        self._r.srem(self.active_user + ":builds", buildId)
+        self._r.srem(user + ":builds", buildId)
 
         #Delete build hash
         self._r.delete(buildId)
 
         #Delete decoration sets
-        for part in self.BUILD_PARTS:
+        for part in build_parts:
             self._r.delete(buildId + ':' + part)
-
-    def get_all_builds(self):
-        return self._r.smembers(self.active_user + ':builds')
+    
+    def get_all_builds(self, user):
+        return self._r.smembers(user)
     
     ####----Build Components (e.g. armor pieces, weapons)----####
 
     BUILD_PARTS = {'head', 'chest', 'arms', 'waist', 'legs', 'weapon'}
 
-    def add_build_component(self, part, itemId):
-        self._r.hset(self.get_build_id(), part, itemId)
+    def add_build_component(self, build_id, part, item_id):
+        self._r.hset(build_id, part, item_id)
 
-    def remove_build_component(self, part):
-        self._r.hdel(self.get_build_id(), part)
-        self._r.delete(self.get_build_id() + ':' + part)
+    def remove_build_component(self, part, build_id):
+        self._r.hdel(build_id, part)
+        self._r.delete(build_id + ':' + part)
     
-    def get_build_parts(self):
-        return self._r.hgetall(self.get_build_id())
+    def get_build_parts(self, build_id):
+        return self._r.hgetall(build_id)
 
     def is_part(self, id, part):
         if (part == 'weapon'):
@@ -127,17 +71,17 @@ class RedisDriver:
     
     ####----Decorations----####
 
-    def add_decoration(self, part, itemId):
-        self._r.sadd(self.get_build_id + ':' + part, itemId)
+    def add_decoration(self, build_id, part, itemId):
+        self._r.sadd(build_id + ':' + part, itemId)
     
-    def remove_decoration(self, part, itemId):
-        self._r.srem(self.get_build_id + ':' + part, itemId)
+    def remove_decoration(self, build_id, part, itemId):
+        self._r.srem(build_id + ':' + part, itemId)
 
-    def get_decorations(self, part):
-        self._r.smembers(self.get_build_id + ':' + part)
+    def get_decorations(self, build_id, part):
+        self._r.smembers(build_id + ':' + part)
 
-    def remove_all_decorations(self, part):
-        self._r.delete(self.get_build_id + ':' + part)
+    def remove_all_decorations(self, build_id, part):
+        self._r.delete(build_id + ':' + part)
 
     def is_decoration(self, id):
         return self._r.sismember('decoration_ids', id)
@@ -147,10 +91,7 @@ class RedisDriver:
     ITEM_TYPES = {'armor', 'weapon', 'skill', 'item', 'decoration'}
 
     def get_object_name(self, id, type_):
-        result = self._r.hget(type_+ '_ids', id)
-        if result is None:
-            raise ValueError(id + ' is not a valid ' + type_ + ' ID.')
-        return result
+        return self._r.hget(type_+ '_ids', id)
     
     def search_object_name(self, name, type_):
         return self._r.hscan_iter(type_ + '_names', '*' + name + '*')
@@ -162,10 +103,7 @@ class RedisDriver:
         return None
     
     def get_object_id(self, name, type_):
-        result = self._r.hget(type_+ '_names', name)
-        if result is None:
-            raise ValueError(name + ' is not a valid ' + type_ + ' name.')
-        return result
+        return self._r.hget(type_+ '_names', name)
     
     def is_object(self, id, type_):
         return self._r.hget(type_+ '_ids', id) is not None
