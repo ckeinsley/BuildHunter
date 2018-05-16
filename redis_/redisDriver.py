e563a098d63e427e1522c286caf2c7466843d8ed
Collin Timothy Moore <collin.moore96@gmail.com>
2018-05-16 15:47:37 -0400

    forgot to remove prints

--- redis_/redisDriver.py
+++ redis_/redisDriver.py
@@ -61,9 +61,7 @@ class RedisDriver:
         self._r.hset(build_id, part, item_id)
 
     def remove_build_component(self, build_id, part):
-        print(build_id, part)
         result = self._r.hdel(build_id, part)
-        print(result)
         self._r.delete(build_id + ':' + part)
     
     def get_build_parts(self, build_id):
