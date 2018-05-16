4eeccd8c6ccdfd0b5d37d994d5ed6f526ab1db05
cKeinsley <keinsleyChris@gmail.com>
2018-05-16 15:08:07 -0400

    moved weapon parsing into separate files

---
+++ cassandra/cassandra_script_weapon_import.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Standard Imports
+import sys
+from pprint import pprint
+
+# Object Loader
+sys.path.insert(0,'../WebScrapper')
+from obj_loader import read_weapon_file
+
+# Cassandra Driver
+import cassandraDriver as db
+KEYSPACE = 'testkeyspace'
+
+def main():
+    print("Connecting to Cassandra")
+    db.connect()
+    print("Connected")
+    db.createWeaponTable()
+    print("Begining Load")
+    weapon_list = read_weapon_file()
+    print("Load Complete")
+    
+    print("Dumping blademaster Items")
+    blademaster_list = weapon_list.get('Blademaster')
+
+    for weapon in blademaster_list:
+        print("Processing " + weapon.get('Name'))
+        if "dummy" in weapon.get('Name'):
+            continue
+        db.insertWeapon(weapon)
+
+
+    print("Dumping Gunner Items")
+    gunner_list = weapon_list.get('Gunner')
+    for weapon in gunner_list:
+        print("Processing " + weapon.get('Name'))
+        if "dummy" in weapon.get('Name'):
+            continue
+        db.insertWeapon(weapon)
+        
+    print("Finished Dumping Files")
+
+
+"""
+{'Affinity': '15',
+ 'Attack': '720',
+ 'Create_Items': [],
+ 'Create_Price': None,
+ 'Defense': None,
+ 'Element': {'Name': ' Poison ', 'Value': ' 300 '},
+ 'Glaive_Type': None,
+ 'Name': 'Chrome Razor',
+ 'Phial': None,
+ 'Rarity': '4',
+ 'Shelling': None,
+ 'Slot': 1,
+ 'True_Attack': '150',
+ 'Upgrade_Items': [{'Name': 'G.Jaggi Claw+', 'Quantity': '3', 'id': 390},
+                   {'Name': 'Carbalite Ore', 'Quantity': '2', 'id': 212},
+                   {'Name': 'Toxin Sac', 'Quantity': '3', 'id': 332},
+                   {'Name': 'Bird Wyvern Gem', 'Quantity': '1', 'id': 324}],
+ 'Upgrade_Price': '20,000z',
+ 'Upgrades_To': [{'Name': 'Chrome Quietus', 'id': 5011}],
+ 'Weapon_Family': 'Great Sword',
+ 'id': 5010}
+"""
+
+
+if __name__ == "__main__":
+    main()
---
+++ cassandra/weapon_conversion.py
@@ -0,0 +1,75 @@
+def convertWeapon(weapon):
+    blademaster = __isBlademaster(weapon)
+    newWeapon = {}
+    newWeapon['affinity'] = __intOrNone(weapon.get('Affinity'))
+    newWeapon['attack'] = __intOrNone(weapon.get('Attack'))
+    newWeapon['create_price'] = __filterString(weapon.get('Create_Price'))
+    newWeapon['defense'] = __intOrNone(weapon.get('Defense'))
+    newWeapon['glaive_type'] = __filterString(weapon.get('Glaive_Type'))
+    newWeapon['name'] = __filterString(weapon.get('Name'))
+    newWeapon['phial'] = __filterString(weapon.get('Phial'))
+    newWeapon['rarity'] = __intOrNone('Rarity')
+    newWeapon['shelling'] = __filterString(weapon.get('Shelling'))
+    newWeapon['slot'] = __intOrNone(weapon.get('Slot'))
+    newWeapon['true_attack'] = __intOrNone(weapon.get('True_Attack'))
+    newWeapon['upgrade_price'] = __filterString(weapon.get('Upgrade_Price'))
+    newWeapon['weapon_family'] = __filterString(weapon.get('Weapon_Family'))
+    newWeapon['id'] = __intOrNone(weapon.get('id'))
+    if blademaster:
+        newWeapon['class'] = 'Blademaster'
+    else:
+        newWeapon['class'] = 'Gunner'
+    return newWeapon 
+
+def __isBlademaster(weapon):
+    weapon_family = weapon.get('Weapon_Family')
+    gun = ["Bow","Light Bowgun", "Heavy Bowgun"]
+    return not weapon_family in gun
+
+def convertCreateItems(weapon):
+    items = []
+    wep_id = __intOrNone(weapon.get('id'))
+    for create_item in weapon.get('Create_Items'):
+        item_map = {}
+        item_map['id'] = wep_id
+        item_map['name'] = __filterString(create_item['Name'])
+        item_map['quantity'] = __intOrNone(create_item['Quantity'])
+        item_map['item_id'] = __intOrNone(create_item['id'])
+        items.append(item_map)
+    return items
+
+def convertUpgradeItems(weapon):
+    items = []
+    wep_id = __intOrNone(weapon.get('id'))
+    for upgrade_item in weapon.get('Upgrade_Items'):
+        item_map = {}
+        item_map['id'] = wep_id
+        item_map['name'] = __filterString(upgrade_item['Name'])
+        item_map['quantity'] = __intOrNone(upgrade_item['Quantity'])
+        item_map['item_id'] = __intOrNone(upgrade_item['id'])
+        items.append(item_map)
+    return items
+
+def convertUpgradesTo(weapon):
+    items = []
+    wep_id = __intOrNone(weapon.get('id'))
+    for upgrade_item in weapon.get('Upgrades_To'):
+        item_map = {}
+        item_map['id'] = wep_id
+        item_map['name'] = __filterString(upgrade_item['Name'])
+        item_map['item_id'] = __intOrNone(upgrade_item['id'])
+        items.append(item_map)
+    return items
+
+
+def __filterString(someString):
+    if someString:
+        return someString.replace("'", "''")
+    else:
+        return None
+
+def __intOrNone(possibleInt):
+    try:
+        return int(possibleInt)
+    except:
+        return -1
